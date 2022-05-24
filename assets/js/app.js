c76235d1af055d34de415f4283a84261bbd6fff2
NatanD <natandav39@gmail.com>
2022-05-24 08:40:41 +0200

    Fin

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,30 +3,42 @@ var sidebarBody = document.querySelector('#Nhamburger-sidebar-body');
 var button = document.querySelector('#Nhamburger-button');
 var buttonclose = document.querySelector('#Nhamburger-button-close');
 var overlay = document.querySelector('#Nhamburger-overlay');
+var burger = document.querySelector('#Nhamburger');
 var activatedClass = 'Nhamburger-activated'
 
 sidebarBody.innerHTML = content.innerHTML
 
-button.addEventListener('click', function (e) {
-    e.preventDefault();
 
-    this.parentNode.classList.add(activatedClass);
-    sidebarBody.classList.remove('none');
+button.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  burger.classList.add(activatedClass);
+  sidebarBody.classList.remove('none');
 });
 
 buttonclose.addEventListener('click', function (e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    // this.parentNode.classList.remove(activatedClass);
-    document.getElementById('Nhamburger').classList.remove(activatedClass);
-    sidebarBody.classList.add('none');
+  // this.parentNode.classList.remove(activatedClass);
+  burger.classList.remove(activatedClass);
+  sidebarBody.classList.add('none');
 });
 
+// Nav Bar Background
+
+window.onscroll = () => {
+
+  if (window.scrollY >= 860) {
+    content.classList.add('navBackground');
+  } else {
+    content.classList.remove('navBackground');
+  }
+}
 
 // Loader
 
 window.onload = () => {
-    setTimeout(() => {
-      document.getElementById('Nparents').classList.add('none')
-    }, 2500);
-  }
+  setTimeout(() => {
+    document.getElementById('Nparents').classList.add('none')
+  }, 2500);
+}
